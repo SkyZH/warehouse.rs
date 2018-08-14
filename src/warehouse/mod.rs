8a4fe2a2140f4499a8610134116406bb75faed9d
SkyZH <iskyzh@gmail.com>
2018-08-14 17:41:27 +0800

    initial commit

---
+++ src/warehouse/mod.rs
@@ -0,0 +1,4 @@
+pub mod command;
+pub mod object;
+mod storage;
+pub use self::storage::Storage;
---
+++ src/warehouse/object/mod.rs
@@ -0,0 +1,9 @@
+mod bot;
+mod shelf;
+mod site;
+mod object;
+
+use self::bot::Bot;
+use self::shelf::Shelf;
+use self::site::Site;
+use self::object::Object;
