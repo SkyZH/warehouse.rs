e172c61d94e559a815da4ace21d415e78baac17c
SkyZH <iskyzh@gmail.com>
2018-08-21 16:49:16 +0800

    add world data

--- src/worlddata/one_bot.rs
+++ src/worlddata/one_bot.rs
@@ -1,6 +1,6 @@
 use warehouse::World;
 use warehouse::object::{ Bot, Shelf, Site, Location, Object };
-use worlddata::util::move_location;
+use worlddata::util::{ move_location, map_as_object };
 use std::sync::{ Arc, Mutex };
 
 pub fn one_bot() -> (World, 
@@ -11,7 +11,6 @@ pub fn one_bot() -> (World,
     let (MARGIN_L, MARGIN_T) = (1, 1);
     let mut world = World::new();
     let mut vec_shelf : Vec<Arc<Mutex<Shelf>>> = Vec::new();
-    let mut vec_bot : Vec<Arc<Mutex<Bot>>> = Vec::new();
     let mut vec_site : Vec<Arc<Mutex<Site>>> = Vec::new();
     for cnt in 0..SHELF_COUNT {
         for col in 0..2 {
@@ -23,10 +22,14 @@ pub fn one_bot() -> (World,
                 }
             }
         }
+        let mut site = Site::new();
+        move_location(site.clone(), Location::new(cnt * 3 + 1 + MARGIN_L, MARGIN_T + SHELF_WIDTH + 1, 0));
+        vec_site.push(site);
     }
-    let mut bot = Bot::new();
+    world.add_items(map_as_object(&vec_shelf));
+    world.add_items(map_as_object(&vec_site));
+    let bot = Bot::new();
     move_location(bot.clone(), Location::new(MARGIN_L, SHELF_WIDTH + 2, 0));
-    world.add_items(vec_shelf.iter().cloned().map(|obj| obj as Arc<Mutex<Object>>).collect());
     world.add_items(vec![bot.clone()]);
-    (world, vec![bot], vec_shelf, Vec::new())
+    (world, vec![bot], vec_shelf, vec_site)
 }
--- src/worlddata/util.rs
+++ src/worlddata/util.rs
@@ -6,3 +6,7 @@ pub fn move_location(obj: Arc<Mutex<Object>>, location: Location) {
     let loc = obj.get_location();
     *loc = location;
 }
+
+pub fn map_as_object <'a, T: 'a + Object> (vec: &Vec<Arc<Mutex<T>>>) -> Vec<Arc<Mutex<Object + 'a>>>{
+    vec.iter().cloned().map(|obj| obj as Arc<Mutex<Object>>).collect()
+}
